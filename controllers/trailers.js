bc2a1982a84c754c408c42e353050cdba05797db
Andrea Mckenzie <ma3east@gmail.com>
2015-09-15 14:47:42 +0100

    backend not complete but doing a bit on the frontend so can add data via a form

--- controllers/trailers.js
+++ controllers/trailers.js
@@ -6,9 +6,13 @@ var parser = require('xml2json');
 var Movie = require('../models/movie');
 var Trailer = require('../models/trailer');
 
+// variables to use with xml2json
+var myJson;
+var options = {
+  object: true,
+};
 
-
-//search for trailers from traileraddict website - in progess
+//search for trailers from traileraddict api - working and parsing json
 router.get('/:search', function(req, res){
   //console.log(req.params.search);
 
@@ -16,37 +20,33 @@ router.get('/:search', function(req, res){
   var searchTerm = encodeURIComponent(req.params.search);
 
   request(trailerAddict+searchTerm+"&count=5&width=640&width=000", function(err, response, trailer){
+    myJson = parser.toJson(trailer, options);
+
     if(!err && response.statusCode === 200){
-      console.log("data received" + trailer);
-
-      var options = {
-        object: true
-      };
-
-      var json = parser.toJson(trailer, options);
-      console.log(json);
-
-      res.send(json);
+      res.send(myJson);
     } else {
       console.log(err);
     }
   });
 });
 
-//get list of all trailers - working
+//get list of all trailers - 
 router.get('/', function(req, res){
   Trailer.find().populate('movieId similarId').exec(function(err, trailers){
+    // myJson = parser.toJson(trailers, options);
+
     if (err) {
-      res.json({ err: err, message: 'Something went wrong there are no trailers.!'});
+      res.send({ err: err, message: 'Something went wrong there are no trailers.!'});
     } else {
-      res.json(trailers);
+      res.send(trailers);
     }
   });
 });
 
-//get single trailer by id
+//get single trailer by id - ?
 router.get('/:trailer_id', function(req, res){
   Trailer.findById(req.params.trailer_id).populate('movieId similarId').exec(function(err, trailer){
+
     if (err){
       res.send(err);
     } else {
--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,27 @@
-// this will be for the angular  modules and states
+angular
+.module('TrailersApp', ['ui.router'])
+.config(MainRouter)
 
+function MainRouter($stateProvider, $urlRouterProvider){
+  $stateProvider
+  
+  .state('homepage', {
+    url: '/',
+    templateUrl: "templates/homepage/homepage.html"
+  })
+  .$state('about', {
+    url: '/about',
+    templateUrl: "templates/shared/about.html"
+  })
+
+
+
+  $urlRouterProvider.otherwise('/');
+}
+
+
+
+
+
+// this will be for the angular  modules and states
 console.log("yo for the app.js on the front end");
